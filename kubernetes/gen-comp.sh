0339ca556b06dfb4773429b40be2463ab88b7f87
Jirayu Thapadit <dragondarkon@hotmail.com>
2020-03-16 14:42:16 +0700

    add

--- kubernetes/gen-comp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat ns.yaml \
-registry-secret.yaml \
-deployment.yaml \
-service.yaml \
-ingress.yaml \
-> completed.yml 
--- src/handlers/healthz.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import (
-  "net/http"
-)
-
-func healthz(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
